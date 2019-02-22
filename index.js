b0415ff57e6986bd5c36fcf89cbded5990e585ac
Blake Cerecero <Blake@DigitalBlake.com>
2019-02-21 23:07:27 -0600

    [js, gitignore] Added WordPress export script

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const knex_query = knex({
 	},
 })
 
-function joomla_articles(joomla_articles_table, live_domain = 'http://localhost/', output_dir = './_output/articles/') {
+function joomla_articles(table, live_domain = 'http://localhost/', output_dir = './_output/articles/') {
 	knex_query
-		.from(joomla_articles_table)
+		.from(table)
 		.select('id', 'title', 'alias', 'introtext', 'fulltext')
 		.then(rows => {
 			rows.forEach(row => {
@@ -51,5 +51,49 @@ function joomla_articles(joomla_articles_table, live_domain = 'http://localhost/
 			})
 		})
 }
+// joomla_articles('xer2k_content')
 
-joomla_articles('xer2k_content')
+function wordpress_articles(table, live_domain = 'http://localhost/', output_dir = './_output/posts/') {
+	knex_query
+		.from(table)
+		.where(function() {
+			this.where('post_type', 'post').orWhere('post_type', 'page')
+		})
+		.select('id', 'post_title', 'post_content', 'post_name', 'post_type')
+		.then(rows => {
+			rows.forEach(row => {
+				const body_content = `${row.post_content}`
+				const fixed_urls = body_content.replace(/(href|src)="(?!http|https|mailto)/gm, `$1="${live_domain}`)
+
+				const html_content = `
+					<!DOCTYPE html>
+					<html lang="en">
+					<head>
+						<meta charset="UTF-8">
+					</head>
+					<body>
+						<p>Site URL: <a href="${live_domain}?p=${row.id}">${live_domain}?p=${row.id}</a></p>
+						<hr/>
+						<h1>${row.post_title}</h1>
+						${fixed_urls}
+					</body>
+					</html>
+				`
+
+				fs.mkdirs(output_dir)
+
+				// Arguments can be either a single String or in an Array
+				const args = `-f html -t docx -o ${output_dir}${row.id}--${row.post_type}--${row.post_name}.docx`
+
+				// Set your callback function
+				const callback = (err, result) => {
+					if (err) console.error('Oh Nos: ', err)
+					return console.log(result), result
+				}
+
+				// Call pandoc
+				pandoc(html_content, args, callback)
+			})
+		})
+}
+// wordpress_articles('cmbu_posts')
